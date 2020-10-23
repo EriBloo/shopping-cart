6b5c243bc98ad735d82a4d115a60b459811ead43
eriBloo <19932449+EriBloo@users.noreply.github.com>
2020-10-23 10:42:25 +0200

    Updates

---
+++ precache-manifest.de11439c3c23025e2ddeff03c1beda1e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e5d723e787371b863016319ca5d5a3",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "691383577bc6ffc64c66",
+    "url": "/shopping-cart/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6e17ae7b4bf50c16c17e",
+    "url": "/shopping-cart/static/css/main.17bbee28.chunk.css"
+  },
+  {
+    "revision": "691383577bc6ffc64c66",
+    "url": "/shopping-cart/static/js/2.5b5cd7c4.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/shopping-cart/static/js/2.5b5cd7c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e17ae7b4bf50c16c17e",
+    "url": "/shopping-cart/static/js/main.616cdf9e.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  },
+  {
+    "revision": "856ad6173e4afd9829edc5eda41e8d05",
+    "url": "/shopping-cart/static/media/blue.856ad617.jpg"
+  },
+  {
+    "revision": "ffd44f64268128c23f8f0de3da1e498b",
+    "url": "/shopping-cart/static/media/brown.ffd44f64.jpg"
+  },
+  {
+    "revision": "e6fb368351c6e32c28ab35587474ee37",
+    "url": "/shopping-cart/static/media/gray.e6fb3683.jpg"
+  },
+  {
+    "revision": "68987cb7d087a1371ba5e509518e0971",
+    "url": "/shopping-cart/static/media/white.68987cb7.jpg"
+  }
+]);
